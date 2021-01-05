209cc8062b1c9b4193bf83db5f9356b7e81efe2b
Myouuu <meriem.mrabent@hetic.net>
2021-01-05 16:48:54 +0100

    some updates :lipstick:

--- main.go
+++ main.go
@@ -2,23 +2,40 @@ package main
 
 import (
 	"fmt"
+<<<<<<< Updated upstream
 	"strconv"
 
 	"github.com/HETIC-MT-P2021/CQRSES_GROUP1/models"
 	"github.com/joho/godotenv"
+=======
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+
+	"CQRSES/controllers"
+	"CQRSES/seed"
+>>>>>>> Stashed changes
 )
-
+var server = controllers.Server{}
 func main() {
-	fmt.Println("Starting Bot")
 
-	env, _ := godotenv.Read(".env")
 
-	dbPort, dbErr := strconv.ParseInt(env["DB_PORT"], 10, 64)
+		var err error
+		err = godotenv.Load()
+		if err != nil {
+			log.Fatalf("Error getting env, not comming through %v", err)
+		} else {
+			fmt.Println("We are getting the env values")
+		}
 
-	if dbErr != nil {
-		panic(dbErr)
-	}
+		server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
 
-	models.ConnectToDB(env["DB_HOST"], env["DB_NAME"], env["DB_USER"], env["DB_PASSWORD"], dbPort)
+		seed.Load(server.DB)
 
+<<<<<<< Updated upstream
 }
+=======
+		server.Run(":8080")
+
+	}
+>>>>>>> Stashed changes
